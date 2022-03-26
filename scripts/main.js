f5e3fa498ae1c68ca24ce9b6eab38070f4f1db86
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-03-25 20:03:50 -0500

    bug

--- scripts/main.js
+++ scripts/main.js
@@ -22,47 +22,48 @@ const toggleEverything = () => {
 }
 burger.addEventListener('click', toggleEverything)
 navOverlay.addEventListener('click', toggleEverything)
+
+// Scroll to top logic
+const scrollToTopButton = document.getElementById("js-top");
+
+const scrollFunc = () => {
+  let y = window.scrollY;
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 10);
+  }
+};
+
+scrollToTopButton.onclick = function (e) {
+  e.preventDefault();
+  scrollToTop();
+};
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 0,
-    speed: 1000,
-    loop: true,
-    autoplay: false,
-    slidesPerView: 1,
-    // disableOnInteraction: true,
-    // centeredSlides: true,
-    pagination: false,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-next-custom",
-        prevEl: ".swiper-prev-custom",
-    },
+  spaceBetween: 0,
+  speed: 1000,
+  loop: true,
+  autoplay: false,
+  slidesPerView: 1,
+  // disableOnInteraction: true,
+  // centeredSlides: true,
+  pagination: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-next-custom",
+    prevEl: ".swiper-prev-custom",
+  },
 });
-    // Scroll to top logic
-    const scrollToTopButton = document.getElementById("js-top");
-
-    const scrollFunc = () => {
-        let y = window.scrollY;
-        if (y > 0) {
-            scrollToTopButton.className = "top-link show";
-        } else {
-            scrollToTopButton.className = "top-link hide";
-        }
-    };
-
-    window.addEventListener("scroll", scrollFunc);
-
-    const scrollToTop = () => {
-        const c = document.documentElement.scrollTop || document.body.scrollTop;
-        if (c > 0) {
-            window.requestAnimationFrame(scrollToTop);
-            window.scrollTo(0, c - c / 10);
-        }
-    };
-
-    scrollToTopButton.onclick = function (e) {
-        e.preventDefault();
-        scrollToTop();
-    };
